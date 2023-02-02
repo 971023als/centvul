09d8668d09d2ab086cccdca5feaa4142a0a164f1
971023als <971023als@naver.com>
2023-02-02 13:07:58 +0900

    ok

--- U-04.sh
+++ U-04.sh
@@ -26,11 +26,33 @@ EOF
 BAR
 
 
-# Check if the shadow password file exists
-if [ ! -f /etc/shadow ]; then
-    WARN "쉐도우 패스워드 파일이 없습니다. 암호는 암호화되지 않고 저장되지 않습니다"
+STATUS1=FALSE
+STATUS2=FALSE
+CheckIsEncrypted() {
+	grep '^root' $1 | awk -F: '{print $2}' | egrep -q '^\$1\$|^\$2a\$|^\$5\$|^\$6\$'
+	if [ $? -eq 0 ]; then
+		OK $1 파일안에 암호가 암호화 되어 있습니다.
+		STATUS1=TRUE
+	else
+		WARN $1 파일안에 암호가 암호화 되어있지 않습니다.
+	fi
+}
+
+PASSFILE=/etc/passwd
+SHADOWFILE=/etc/shadow
+if [ -f $SHADOWFILE ]; then
+	OK $SHADOWFILE 파일을 사용하고 있습니다.
+	CheckIsEncrypted $SHADOWFILE
+	STATUS2=TRUE
 else
-    OK "쉐도우 패스워드 파일이 있습니다. 암호는 섀도 암호를 사용하여 암호화되고 저장됩니다."
+	WARN $SHADOWFILE 파일을 사용하지 않습니다.
+	CheckIsEncrypted $PASSFILE
+fi
+
+if [ $STATUS1 = 'TRUE' -a $STATUS2 = 'TRUE' ]; then
+	OK [최종] 쉐도우 패스워드를 사용하거나, 패스워드를 암호화하여 저장하는 경우
+else
+	WARN [최종] 쉐도우 패스워드를 사용하지 않고, 패스워드를 암호화하여 저장하지 않는 경우
 fi
 
 
