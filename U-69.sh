479a78d1a7419b0eb7057e7179f616f21fe4adbf
971023als <971023als@naver.com>
2023-02-03 19:40:10 +0900

    Update U-69.sh

--- U-69.sh
+++ U-69.sh
@@ -26,25 +26,26 @@ BAR
 
 nfs_settings_file="/etc/exports"
 
-# Check if the file exists
+
+# 파일이 있는지 확인하십시오
 if [ ! -f $nfs_settings_file ]; then
   WARN "nfs_settings 파일이 존재하지 않습니다. 확인해주세요."
 fi
 
-# Check owner of the file
-if [ $(stat -c '%U' $nfs_settings_file) = "root" ]; then
-  WARN "nfs_settings의 소유자는 루트입니다. 이것은 허용되지 않습니다."
+# 파일 소유자 확인
+if [[ $(stat -c '%U' $nfs_settings_file) = "root" ]]; then
+  OK "nfs_settings의 소유자는 루트입니다. 이것은 허용됩니다."
+else
+  OK "nfs_settings의 소유자는 루트가 아닙니다. 이것은 허용되지 않습니다."
 fi
 
-# Check permission on the file
+# 파일에 대한 사용 권한 확인
 
-if [ $(stat -c '%a' $nfs_settings_file) -lt 644 ]; then
-  WARN "nfs_settings에 대한 권한이 644보다 작습니다. 이것은 허용되지 않습니다."
+if [[ $(stat -c '%a' $nfs_settings_file) -lt 644 ]]; then
+  OK "nfs_settings에 대한 권한이 644보다 작습니다. 이것은 허용됩니다."
+else
+  WARN "nfs_settings에 대한 권한이 644보다 큽니다. 이것은 허용되지 않습니다."
 fi
-
-OK "NFS 접근제어 설정파일의 소유자가 root 이고, 권한이 644 이하인 상태입니다."
-
-
 
 cat $result
 
