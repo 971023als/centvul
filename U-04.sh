d8cc9f3e8509cf4f470164ef58f8f2403db01f67
971023als <971023als@naver.com>
2023-02-06 12:48:35 +0900

    ok

--- U-04.sh
+++ U-04.sh
@@ -26,33 +26,20 @@ EOF
 BAR
 
 
-STATUS1=FALSE
-STATUS2=FALSE
-CheckIsEncrypted() {
-	grep '^root' $1 | awk -F: '{print $2}' | egrep -q '^\$1\$|^\$2a\$|^\$5\$|^\$6\$'
-	if [ $? -eq 0 ]; then
-		OK $1 파일안에 암호가 암호화 되어 있습니다.
-		STATUS1=TRUE
+FILENAME1=/etc/shadow
+FILENAME2=/etc/passwd
+
+
+if [ -f $FILENAME ] ; then
+	INFO "쉐도우 파일이 존재합니다."
+	CHECK=$(cat $FILENAME2 | awk -F: '{print $2}' | grep -v 'x')
+	if [ -z $CHECK ] ; then
+		OK "쉐도우 패스워드를 사용하거나, 패스워드를 암호화하여 저장하는 경우"
 	else
-		WARN $1 파일안에 암호가 암호화 되어있지 않습니다.
+		WARN "쉐도우 패스워드를 사용하지 않고, 패스워드를 암호화하여 저장하지 않는 경우"
 	fi
-}
-
-PASSFILE=/etc/passwd
-SHADOWFILE=/etc/shadow
-if [ -f $SHADOWFILE ]; then
-	OK $SHADOWFILE 파일을 사용하고 있습니다.
-	CheckIsEncrypted $SHADOWFILE
-	STATUS2=TRUE
 else
-	WARN $SHADOWFILE 파일을 사용하지 않습니다.
-	CheckIsEncrypted $PASSFILE
-fi
-
-if [ $STATUS1 = 'TRUE' -a $STATUS2 = 'TRUE' ]; then
-	OK [최종] 쉐도우 패스워드를 사용하거나, 패스워드를 암호화하여 저장하는 경우
-else
-	WARN [최종] 쉐도우 패스워드를 사용하지 않고, 패스워드를 암호화하여 저장하지 않는 경우
+	INFO "쉐도우 파일이 존재하지 않습니다."
 fi
 
 
--- U-21.sh
+++ U-21.sh
@@ -30,30 +30,27 @@ EOF
 
 BAR
 
+CHECK1=$(ls -alL /etc/xinetd.d/* | egrep 'rsh|rlogin|rexec' | egrep -v 'grep|klogin|kshell|kexec' | wc -l)
+CHECK2=$(ls -alL /etc/xinetd.d/* | egrep 'rsh|rlogin|rexec' | egrep -v 'grep|klogin|kshell|kexec' |awk '{print $NF}' >> /root/linuxs/U-21.txt)
 
-# rlogin 서비스가 사용하도록 설정되었는지 확인하십시오
-if [ "$(service is-enabled rlogin)" = "enabled" ]; then
-  WARN "rlogin 서비스가 활성화되었습니다"
+FILE1=$(cat U-21.txt | sed -n '1p')
+FILE2=$(cat U-21.txt | sed -n '2p')
+FILE3=$(cat U-21.txt | sed -n '3p')
+
+FILECHECK1=$(cat $FILE1 | grep disable | awk '{print $3}')
+FILECHECK2=$(cat $FILE2 | grep disable | awk '{print $3}')
+FILECHECK3=$(cat $FILE3 | grep disable | awk '{print $3}')
+
+if [ $CHECK1 != 0 ] ; then
+	if [ $FILECHECK1 == 'yes' ] && [ $FILECHECK2 == 'yes' ] && [ $FILECHECK3 == 'yes' ] ; then
+		OK "불필요한 r 계열 서비스가 비활성화 되어 있는 경우"
+	else
+		WARN "불필요한 r 계열 서비스가 활성화 되어 있는 경우"
+	fi
 else
-  OK "rlogin 서비스가 비활성화되었습니다."
+	OK "불필요한 r 계열 서비스가 비활성화 되어 있는 경우"
 fi
 
-# rsh 서비스가 사용하도록 설정되었는지 확인하십시오
-if [ "$(service is-enabled rsh)" = "enabled" ]; then
-  WARN "rsh 서비스가 활성화되었습니다"
-else
-  OK "rsh 서비스가 비활성화되었습니다"
-fi
-
-# exec 서비스가 활성화되었는지 확인하십시오
-if [ "$(service is-enabled exec)" = "enabled" ]; then
-  WARN "exec 서비스 사용"
-else
-  OK "exec 서비스가 비활성화됨"
-fi 
-
-
-  
 
 cat $result
 
