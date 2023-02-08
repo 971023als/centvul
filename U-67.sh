571a38ef98c538a0ab665628269af98e062a25f5
971023als <971023als@naver.com>
2023-02-08 10:58:52 +0900

    Update U-67.sh

--- U-67.sh
+++ U-67.sh
@@ -23,23 +23,23 @@ EOF
 
 BAR
 
-snmpd_config_file="/path/to/snmpd.conf"
+snmpd_config_file="/etc/snmp/snmpd.conf"
 
-# Check if the snmpd.conf file exists
-if [ ! -f $snmpd_config_file ]; then
-  INFO "snmpd.conf 파일이 없습니다. 확인해주세요."
-fi
-
-# Search for community names in the snmpd.conf file
+# snmpd.conf 파일에서 커뮤니티 이름 검색
 communities=$(grep -E '^community' $snmpd_config_file | cut -d ' ' -f 2)
 
-for community in $communities; do
-  if [ $community == "public" ] || [ $community == "private" ]; then
-    WARN "Community name $community는 허용되지 않습니다."
-  fi
+# snmpd.conf 파일이 있는지 확인합니다
+if [ ! -f $snmpd_config_file ]; then
+  INFO "snmpd.conf 파일이 없습니다. 확인해주세요."
+else
+  for community in $communities; do
+    if [ $community == "public" ] || [ $community == "private" ]; then
+      WARN "Community name $community 는 허용되지 않습니다."
+    else
+      OK "Community name $community 는 허용되고 있습니다." 
+    fi
+fi
 done
-
-OK "SNMP Community 이름이 public, private 이 아닌 경우입니다."
 
 cat $result
 
