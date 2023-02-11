9ac18a97cc040ab6dd6c9f38969a8f19c95eaeb9
971023als <971023als@naver.com>
2023-02-12 01:33:48 +0900

    Update U-13.sh

--- U-13.sh
+++ U-13.sh
@@ -29,7 +29,7 @@ do
   IFS=':' read -r -a fields <<< "$line"
 
   # 홈 디렉토리에서 SUID 또는 SGID 권한이 있는 파일 확인
-  output=$(find -user root -type f \( -perm -04000 -o -perm -02000 \) -exec ls -al {} \;)
+  output=$(find / -user root -type f \( -perm -04000 -o -perm -02000 \) -exec ls -al {} \;)
 
   # 출력을 배열로 분할
   arr=($output)
